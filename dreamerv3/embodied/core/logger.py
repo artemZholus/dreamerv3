079171b2f064eb145fc8c7e1085b2c353091d59b
Artem Zholus <artem.zholus@gmail.com>
2023-04-27 13:51:19 -0400

    add wandb

--- dreamerv3/embodied/core/logger.py
+++ dreamerv3/embodied/core/logger.py
@@ -233,9 +233,7 @@ class WandBOutput:
     import wandb
     wandb.init(
         project="dreamerv3",
-        name=logdir.name,
-        # sync_tensorboard=True,,
-        entity='word-bots',
+        name=config.wdb_name,
         config=dict(config),
     )
     self._wandb = wandb
@@ -263,7 +261,7 @@ class WandBOutput:
         # If the video is a float, convert it to uint8
         if np.issubdtype(value.dtype, np.floating):
           value = np.clip(255 * value, 0, 255).astype(np.uint8)
-        bystep[step][name] = wandb.Video(value)
+        bystep[step][name] = wandb.Video(value, format='mp4')
 
     for step, metrics in bystep.items():
       self._wandb.log(metrics, step=step)
