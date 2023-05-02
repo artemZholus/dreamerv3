ab34938c8b8ca19c973ee563d9d0c9daa42fe49c
Artem Zholus <zholus@cedar5.cedar.computecanada.ca>
2023-05-01 20:07:22 -0700

    fix

--- run_cedar.sh
+++ run_cedar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=def-bengioy
-#SBATCH --time=1-00:00
+#SBATCH --account=rrg-bengioy-ad
+#SBATCH --time=3-00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem 100000M
 #SBATCH --gpus-per-node=v100l:1
@@ -12,4 +12,6 @@ module load singularity/3.8
 # cd $SCRATCH
 # singularity pull docker://artemzholus/jax_cuda11.8
 export WANDB_MODE=offline
+export WANDB_ENTITY=chandar-rl
+export WANDB_PROJECT=s4dreamer
 ./job.sh "$@"
